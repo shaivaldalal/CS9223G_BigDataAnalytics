52b60a636e035721a55a4b47b576284a52ebdca2
Shaival Dalal <shaivaldalal@gmail.com>
2017-12-20 00:06:15 -0500

    Final

--- Project/Cleaning.py
+++ Project/Cleaning.py
@@ -13,20 +13,21 @@
 # export PYSPARK2_PYTHON=/share/apps/python/3.4.4/bin/python3
 # export PYSPARK_PYTHON=/share/apps/python/3.4.4/bin/python3
 
-## Preparing pySpark environment ------------------
+# module load java/1.8.0_72
+# module load spark/2.2.0
+# module load python/gnu/3.4.4
+# export PYSPARK2_PYTHON=/share/apps/python/3.4.4/bin/python3
+# export PYSPARK_PYTHON=/share/apps/python/3.4.4/bin/python3
 import time
 import sys	
 from pyspark.sql.functions import *
 from pyspark.sql import SparkSession
 from pyspark import SparkContext
-
 spark = SparkSession.builder.master("local").appName("DataCleaning311").getOrCreate()
 #Reading threshold CSV file. We specify the custom date format by referrign to the data dictionary and specify the nullValue as Unspecified
-data=spark.read.csv(sys.argv[1],header=True,nullValue='Unspecified',dateFormat='mm/dd/yy')
+data=spark.read.csv(sys.argv[1],header=True,nullValue='Unspecified')
+# data=spark.read.csv('Project/new_311.csv',header=True,nullValue='Unspecified',dateFormat='mm/dd/yy h:mm:ss a')
 
-
-
-## Deliverable 1: Code used to identify null values ------------------
 # We create colData to store percentage of missing values in every column
 colData=[]
 length=data.count()
@@ -37,17 +38,16 @@ for columns in data.columns:
 for i in range(0,len(colData)):
 	colData[i]=(colData[i]/length)*100
 
-
-## Deliverable 2: Code used to remove/replace null values ------------------
 # We drop columns when the percent of missing value is greater than the threshold value
 headers=data.columns
 for i in range(0,len(colData)):
 	if(colData[i]>60):
 		data=data.drop(headers[i])
 
-data=data.withColumn("Closed Date",from_unixtime(unix_timestamp("Closed Date","mm/dd/yy h:mm:ss a")))
-data=data.withColumn("Created Date",from_unixtime(unix_timestamp("Created Date","mm/dd/yy h:mm:ss a")))
-data=data.withColumn("Resolution Action Updated Date",from_unixtime(unix_timestamp("Resolution Action Updated Date","mm/dd/yy h:mm:ss a")))
+data=data.withColumn("Closed Date",to_timestamp(col("Closed Date"),"M/d/y h:m:s a"))
+data=data.withColumn("Created Date",to_timestamp(col("Created Date"),"M/d/y h:m:s a"))
+data=data.withColumn("Resolution Action Updated Date",to_timestamp(col("Resolution Action Updated Date"),"M/d/y h:m:s a"))
+
 
 # We create a temp SQL table in Spark to allow for easier querying and data manipulation
 data.createOrReplaceTempView("table")
@@ -70,22 +70,8 @@ data=data.withColumn("Borough", when((col("Borough").isNull()) & (col("Incident 
 data=data.withColumn("Borough", when((col("Borough").isNull()) & (col("Incident Zip")>=11354) & (col("Incident Zip")<=11697),"QUEENS").otherwise(col("Borough")))
 data=data.drop('Location')
 data=data.drop('Park Borough')
+
 data.createOrReplaceTempView("table")
 data=spark.sql("SELECT * FROM TABLE WHERE BOROUGH IS NOT NULL")
 
-
-## Deliverable 3: Code used to report statistical information about null values in columns ------------------
-colData_new=[]
-length_new=data.count()
-for columns in data.columns:
-	colData_new.append(data.select([count(when((col(columns)=="NA") | (col(columns)=="Unspecified") | (col(columns)=="N/A") | (col(columns)=="") | (col(columns).isNull()) | (col(columns)=="0 Unspecified"),columns)).alias(columns)]).take(1)[0][0])
-
-# Maximum number of null values
-max(colData)
-
-# We calculate percentages
-for i in range(0,len(colData_new)):
-	colData_new[i]=(colData_new[i]/length_new)*100
-
-# Save the cleaned dataset
-data.coalesce(1).write.csv("Cleaned_311.csv")
+data.coalesce(1).write.csv("Cleaned_311",header=True)
